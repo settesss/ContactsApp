65e5f8b005260503e7d9ce990b198047c35fb6aa
settesss <nnmanakovv@gmail.com>
2023-03-18 23:01:51 +0700

    feature(Contact.cs): добавлен класс "Контакт" с полями "Полное имя", "e-mail", "Номер телефона", "Дата рождения", "ID ВКонтакте". Добавлены свойства и конструкторы класса. Созданы обработчики исключений.
    feature(Project.cs): добавлен класс "Проект", содержащий список всех контактов и методы по сортировке контактов по полному имени, поиску именинников, поиску контактов по подстроке. Методы будут допиливаться, но свою работу они демонстрируют прекрасно :3
    
    feature(ProjectManager.cs): добавлен пока что пустой класс "Менеджер проекта".
    
    feature(MainForm.cs, ContactForm.cs): добавлен и закомментирован код для проверки правильности классов и дальнейшей реализации :3
    
    ui(MainForm): незначительная доработка. Добавлен BirthdayUsersLabel, в который будет помещаться строка, которую возвращает метод по нахождению именинников :3

---
+++ src/ContactsApp/ContactsApp.Model/Project.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsApp.View
+{
+    internal class Project
+    {
+        private List<Model.Contact> contacts;
+
+        public Project()
+        {
+            contacts = new List<Model.Contact>();
+        }
+
+        public List<Model.Contact> GetContacts()
+        {
+            return contacts;
+        }
+
+        public ListBox FullNameSort(ListBox listBox, string contactName)
+        {
+            ListBox listBoxSorted = new ListBox();
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                if (listBox.Items[i].ToString() == contactName)
+                {
+                    listBoxSorted.Items.Add(listBox.Items[i]);
+                }
+            }
+            return listBoxSorted;
+        }
+
+        public ListBox SubstringSort(ListBox listBox, string substring) 
+        { 
+            ListBox listBoxSorted = new ListBox();
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                if (listBox.Items[i].ToString().Contains(substring))
+                {
+                    listBoxSorted.Items.Add(listBox.Items[i]);
+                }
+            }
+            return listBoxSorted;
+        }
+
+        public string BirthdaySort(ListBox listBox, List<Model.Contact> contacts)
+        {
+            string birthdayList = "";
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                if (contacts[i].BirthDate == DateTime.Today)
+                {
+                    birthdayList += listBox.Items[i].ToString() + "; ";
+                }
+            }
+            return birthdayList;
+        }
+    }
+}
