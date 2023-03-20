faea758c4018fb0176044b7a9ebeac81cd63a75a
settesss <nnmanakovv@gmail.com>
2023-03-20 13:08:15 +0700

    fixes(ContactsApp.sln): исправлено добавление второго проекта в решение :3

--- src/ContactsApp/ContactsApp.Model/Properties/AssemblyInfo.cs
+++ src/ContactsApp/ContactsApp.Model/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("81da32d5-171f-4b6f-afb6-dd1015e99ccc")]
+[assembly: Guid("00dccbfe-fce9-40a4-b24e-7760c7eb7802")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
