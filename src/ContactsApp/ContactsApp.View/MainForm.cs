d01fa74fe076f32e67c01b6907199ca5761f2644
settesss <nnmanakovv@gmail.com>
2023-03-22 00:53:22 +0700

    **feature(ContactFactory.cs): добавлен новый статический класс, в который перенесена программная генерация данных и объявлен метод GenerateRandom, принимающий на вход пустой объект Model.Contact и возвращающий его заполненным :3

--- src/ContactsApp/ContactsApp.View/MainForm.cs
+++ src/ContactsApp/ContactsApp.View/MainForm.cs
@@ -24,56 +24,6 @@ namespace ContactsApp.View
         private Model.Project _project = new Model.Project();
 
         /// <summary>
-        /// Массив полных имен.
-        /// </summary>
-        private string[] _names = { "никита", "артем", "илья", "мария", "анастасия", "елена" };
-
-        /// <summary>
-        /// Массив адресов электронной почты.
-        /// </summary>
-        private string[] _mails = { 
-            "lola2012@gmail.com", 
-            "akiraISdead@mail.ru", 
-            "bubochka1488@bk.ru", 
-            "booker_pooker@gmail.com", 
-            "doglover@gmail.com", 
-            "pautinka@gmail.com" 
-        };
-
-        /// <summary>
-        /// Массив номеров телефона.
-        /// </summary>
-        private string[] _numbers = { 
-            "9069778977", 
-            "9022278977", 
-            "9232211977", 
-            "9069772211", 
-            "9069771488", 
-            "9033378977" 
-        };
-
-        /// <summary>
-        /// Массив дат рождения.
-        /// </summary>
-        private DateTime[] _dates = { 
-            new DateTime(1980, 02, 12), 
-            new DateTime(1956, 12, 27), 
-            new DateTime(1976, 05, 14) 
-        };
-
-        /// <summary>
-        /// Массив уникальных идентификаторов пользователей VK.
-        /// </summary>
-        private string[] _ids = { 
-            "vk.com/locker", 
-            "vk.com/chzh", 
-            "vk.com/pooker", 
-            "vk.com/mikrochel", 
-            "vk.com/sadist", 
-            "vk.com/maria-ra" 
-        };
-
-        /// <summary>
         /// Очищает все элементы ListBox, возвращает обновленный список контактов.
         /// </summary>
         private ListBox UpdateListBox()
@@ -117,11 +67,7 @@ namespace ContactsApp.View
         private void AddContact()
         {
             Model.Contact user = new Model.Contact();
-            user.FullName = _names[new Random().Next(0, _names.Length)];
-            user.Email = _mails[new Random().Next(0, _mails.Length)];
-            user.PhoneNumber = _numbers[new Random().Next(0, _numbers.Length)];
-            user.BirthDate = _dates[new Random().Next(0, _dates.Length)];
-            user.VKID = _ids[new Random().Next(0, _ids.Length)];
+            ContactFactory.GenerateRandom(user);
             _project.Contacts.Add(user);
         }
 
